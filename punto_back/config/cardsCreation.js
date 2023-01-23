886db317256375c2ccfa27d0d05b9939a6e2aab3
Sacha FARINEL <farinel.sacha@tutanota.com>
2023-01-23 23:38:03 +0100

    back change

--- punto_back/config/cardsCreation.js
+++ punto_back/config/cardsCreation.js
@@ -5,7 +5,7 @@ const cardsCreation = async () => {
         Card.countDocuments({}, function (err, count) {
             if (count === 0) {
                 console.log('Cards creation')
-                const colors = ['red', 'blue', 'green', 'yellow'];
+                const colors = ['#e2100a', '#00b4e7', '#feb008', '#6dba0f'];
                 for (let i = 1; i <= 2; i++) {
                     for (const color of colors) {
                         for (let j = 1; j <= 9; j++) {
--- punto_back/controllers/usersController.js
+++ punto_back/controllers/usersController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 const asyncHandler = require('express-async-handler')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 // @desc Get all users
 // @route GET /users
